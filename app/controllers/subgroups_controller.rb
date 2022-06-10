b0cd9b6c0bbe010bf510dc90abbe14bbed40a2a1
Sergio Oseko <osk.sergio@gmail.com>
2022-06-10 07:03:51 -0300

    feat: method update (subgroup controller)

--- app/controllers/subgroups_controller.rb
+++ app/controllers/subgroups_controller.rb
@@ -5,6 +5,10 @@ class SubgroupsController < ApplicationController
     @subgroup = Subgroup.new
   end
 
+  # GET /subgroups/1/edit
+  def edit
+  end
+
   # POST /subgroups or subgroups.json
   def create
     @subgroup = Subgroup.new(subgroup_params)
@@ -15,6 +19,19 @@ class SubgroupsController < ApplicationController
         format.json { render :show, status: :created, location: @subgroup }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @subgroup.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /subgroups/1 or /subgroups/1.json
+  def update
+    respond_to do |format|
+      if @subgroup.update(subgroup_params)
+        format.html { redirect_to subgroup_url(@subgroup), notice: "Subgroup was successfully updated." }
+        format.json { render :show, status: :ok, location: @subgroup }
+      else
+        format.html { render :edit, status: :unprocessable_entity}
         format.json { render json: @subgroup.errors, status: :unprocessable_entity }
       end
     end
