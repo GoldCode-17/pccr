02252861414625aae72f7926025db60b1aa0c843
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-07-14 16:00:18 -0300

    feat: enquadramento

--- app/models/framing.rb
+++ app/models/framing.rb
@@ -1,5 +1,5 @@
 class Framing < ApplicationRecord
   validates :service_time, :position, presence: true
   validates :position, uniqueness: true
-
+  validates :service_time, length: { maximum: 30 }
 end
