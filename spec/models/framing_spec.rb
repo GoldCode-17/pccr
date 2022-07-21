38536b2b0b597703879855391fb03b4d4f91ad7a
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-07-21 08:12:46 -0300

    test: Crud Framing

--- spec/models/framing_spec.rb
+++ spec/models/framing_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Framing, type: :model do
   context 'Crud Framing' do
-    it 'persiste um enquadramento com atributos válidos' do
+
+    it 'persists a framing with valid attributes' do
       framing = Framing.create(service_time: "1 Ano até 3 Anos", position: 1)
 
       expect(framing).to be_persisted
     end
+
+    it 'change a framing with valid attributes' do
+      framing = Framing.create(service_time: "1 Ano até 3 Anos", position: 1)
+
+      expect(framing).to be_persisted
+
+
   end
 end
