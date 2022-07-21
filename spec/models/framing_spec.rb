c43c7e4e18a7816ba6f61a0716383f9993fa7b46
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-07-21 15:44:03 -0300

    test: Crud Framing

--- spec/models/framing_spec.rb
+++ spec/models/framing_spec.rb
@@ -11,9 +11,16 @@ RSpec.describe Framing, type: :model do
 
     it 'change a framing with valid attributes' do
       framing = Framing.create(service_time: "1 Ano até 3 Anos", position: 1)
+      framing.update(service_time: "3 Anos e 1 Dia até 5 Anos", position: 2)
 
       expect(framing).to be_persisted
+    end
 
+    it 'Delete framing' do
+      framing = Framing.create(service_time: "1 Ano até 3 Anos", position: 1)
+      framing.destroy
 
+      expect { framing.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
   end
 end
