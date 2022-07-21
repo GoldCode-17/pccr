b221a393c2a33480b8386c48769ac3c4e4f9de44
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-07-21 00:11:33 -0300

    test: Crud Framing

--- spec/models/framing_spec.rb
+++ spec/models/framing_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Framing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Crud Framing' do
+    it 'persiste um enquadramento com atributos válidos' do
+      framing = Framing.create(service_time: "1 Ano até 3 Anos", position: 1)
+
+      expect(framing).to be_persisted
+    end
+  end
 end
