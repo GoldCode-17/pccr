43b043d415a406a0e5f3eb039d39a9ca43ba7dad
gilson dourado <gilsondourado10@gmail.com>
2022-07-26 00:51:49 -0300

    feat(setup)first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_195156) do
+ActiveRecord::Schema.define(version: 2022_07_26_022409) do
+
+  create_table "schoolings", force: :cascade do |t|
+    t.string "level"
+    t.string "description"
+    t.float "percentage"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "subgroups", force: :cascade do |t|
     t.string "acronym"
